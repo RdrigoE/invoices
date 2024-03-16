5b2e1c86b7115f9570c0a4d4a7967f880fe53660
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2024-03-16 15:36:24 +0000

    entry

--- app/Models/Sheet.php
+++ app/Models/Sheet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sheet extends Model
 {
@@ -13,5 +14,10 @@ class Sheet extends Model
     public function folder(): BelongsTo
     {
         return $this->belongsTo(Folder::class);
+    }
+
+    public function entries(): HasMany
+    {
+        return $this->hasMany(Entry::class);
     }
 }
